d087965749b26f2d3a6fc656463db1767a944008
maccadeez <77422979+maccadeez@users.noreply.github.com>
2024-03-16 11:34:12 -0500

    alb module

--- main.tf
+++ main.tf
@@ -20,16 +20,13 @@ data "aws_vpc" "default" {
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
+  name   = "dev"
+  cidr   = "10.0.0.0/16"
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
@@ -42,22 +39,25 @@ resource "aws_instance" "blog" {
   subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
-    Name = "HelloWorld"
+  Name = "HelloWorld"
   }
 }
 
 module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name            = "blog-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  name               = "blog-alb"
 
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
 
   target_groups = [
    {
-      name_prefix      = "blog"
+      name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
@@ -70,15 +70,16 @@ module "alb" {
     }
   ]
 
-http_tcp_listeners = [
-  port = 80
-  protocol = "HTTP"
-  target_group_key = "blog-"
-
-]
+  http_tcp_listeners = [
+    {
+    port               = 80
+    protocol           = "HTTP"
+    target_group_index = 0
+    }
+  ]
 
   tags = {
-    Environment = "Development"
+    Environment = "dev"
   }
 }
 
